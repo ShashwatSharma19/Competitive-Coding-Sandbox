6f71967ff4b47355a6224c1bf353b3a2ba5bb165
Shashwat Sharma <71481172+ShashwatSharma19@users.noreply.github.com>
2026-08-17 16:31:41 +0530

    Time: 351 ms (56%), Space: 92 MB (24.24%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.py
+++ 1907-count-salary-categories/1907-count-salary-categories.py
@@ -1,12 +1,16 @@
-import pandas as pd
-
 def count_salary_categories(accounts: pd.DataFrame) -> pd.DataFrame:
-    low = (accounts['income']< 20000).sum()
-    avg = ((accounts['income'] >= 20000)& (accounts['income'] <= 50000)).sum()
-    high = (accounts['income'] > 50000).sum()
-
-    result = pd.DataFrame({
-        'category': ['Low Salary', 'Average Salary', 'High Salary'],
-        'accounts_count':[low, avg, high]
-    })
-    return result
+    categories = ['Low Salary', 'Average Salary', 'High Salary']
+    
+    
+    def get_category(income):
+        if income < 20000:
+            return 'Low Salary'
+        elif income <= 50000:
+            return 'Average Salary'
+        else:
+            return 'High Salary'
+    
+    accounts['category'] = accounts['income'].apply(get_category)
+    counts = accounts['category'].value_counts().reindex(categories, fill_value=0)
+    
+    return pd.DataFrame({'category': counts.index, 'accounts_count': counts.values})
